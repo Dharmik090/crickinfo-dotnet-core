0810912ebb00de5d32e1edd4c7ab9a669451c91d
Dharmik090 <dharmikparmarpd@gmail.com>
2024-11-14 11:34:33 +0530

    Update README.md

--- obj/Debug/net8.0/crickinfo_mvc_ef_core.AssemblyInfo.cs
+++ obj/Debug/net8.0/crickinfo_mvc_ef_core.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crickinfo_mvc_ef_core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a60e4faf8363ce93f64f6bff07af8a29827cf2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("crickinfo_mvc_ef_core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crickinfo_mvc_ef_core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
